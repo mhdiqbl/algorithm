3f48239c778adbd75cf1606ee7be1494edb39667
Muhammad Iqbal <miskiqbal@gmail.com>
2022-09-09 22:59:52 +0700

    ArraySum

---
+++ arraysum.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+function simpleArraySum($ar) {
+    // Write your code here
+    
+    
+    // method 1
+    $result = 0;
+    $x = sizeof($ar);
+    for($i=0 ; $i<$x ; $i++){
+        $result+=$ar[$i];
+    }
+    return $result;
+    
+    // method 2
+    $sum=0;
+    foreach($ar as $value){
+       $sum+=$value;
+    }
+    return $sum;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$ar_count = intval(trim(fgets(STDIN)));
+
+$ar_temp = rtrim(fgets(STDIN));
+
+$ar = array_map('intval', preg_split('/ /', $ar_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = simpleArraySum($ar);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
